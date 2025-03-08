03e4dc8072eb97015faddaf48abcc21f616a8405
DipinPreetSingh <2023mt93032@wilp.bits-pilani.ac.in>
2025-03-08 15:47:53 +0530

    Update

---
+++ lib/constants.ts
@@ -0,0 +1,7 @@
+export const APP_NAME =
+  process.env.NEXT_PUBLIC_APP_NAME || 'Total_Office_Solutions'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Your Vision, Our Expertise'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'A modern office and business software solution for small businesses and professionals'
