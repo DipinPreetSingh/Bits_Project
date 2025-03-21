5a907980e46134b5bace86c2b64daa1c7ba2a890
DipinPreetSingh <2023mt93032@wilp.bits-pilani.ac.in>
2025-03-21 21:06:07 +0530

    Update

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,68 @@
 import { HomeCarousel } from '@/components/shared/home/home-carousel'
+import { HomeCard } from '@/components/shared/home/home-card'
+import{ toSlug } from '@/lib/utils'
+import { getAllCategories, getProductsForCard } from '@/lib/actions/product.actions'
 import data from '@/lib/data'
 
-export default async function Page() {
+export default async function HomePage() {
+    const categories = (await getAllCategories()).slice(0, 4)
+    const newArrivals = await getProductsForCard({  
+      tag: 'new-arrival',
+      limit: 4,
+    })
+    const featured = await getProductsForCard({
+      tag: 'featured',
+      limit: 4,
+    })
+    const bestSellers = await getProductsForCard({
+      tag: 'best-seller',
+      limit: 4,
+    })
+    const cards = [
+      {
+        title: 'Categories to explore',
+        link: {
+          text: 'See More',
+          href: '/search',
+        },
+        items: categories.map((category) => ({
+          name: category,
+          image: `/images/${toSlug(category)}.jpg`,
+          href: `/search?category=${category}`,
+        })),
+      },
+      {
+        title: 'Explore New Arrivals',
+        items: newArrivals,
+        link: {
+          text: 'View All',
+          href: '/search?tag=new-arrival',
+        },
+      },
+      {
+        title: 'Discover Best Sellers',
+        items: bestSellers,
+        link: {
+          text: 'View All',
+          href: '/search?tag=best-sellers',
+        },
+      },
+      {
+        title: 'Featured Products',
+        items: featured,
+        link: {
+          text: 'Shop Now',
+          href: '/search?tag=featured-products',
+        },
+      },
+    ]
+  
     return (
-      <HomeCarousel items={data.carousels} />
+      <>
+        <HomeCarousel items={data.carousels} />
+        <div className='md:p-4 md:space-y-4 bg-border'>
+          <HomeCard cards={cards} />
+        </div>
+      </>
     )
   }
-
